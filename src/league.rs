4b37342a08906669ad31988f2fd794fd9ecf224b
whatsacomputertho <ebalcik71@gmail.com>
2025-05-18 18:28:06 -0400

    feat(season): implement initial league season functionality

--- src/league.rs
+++ src/league.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeMap;
 
+use chrono::Datelike;
 use serde::{Serialize, Deserialize};
 
 /// # `LeagueTeam` struct
@@ -84,6 +85,85 @@ impl LeagueTeam {
     }
 }
 
+/// # `LeagueSeason` struct
+///
+/// A `LeagueSeason` represents a season of a football league.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Default, Serialize, Deserialize)]
+pub struct LeagueSeason {
+    year: usize,
+    complete: bool
+}
+
+impl LeagueSeason {
+    /// Constructor for the `LeagueSeason` struct, with the year
+    /// defaulting to the current year
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::LeagueSeason;
+    ///
+    /// let my_league_season = LeagueSeason::new();
+    /// ```
+    pub fn new() -> LeagueSeason {
+        LeagueSeason{
+            year: chrono::Utc::now().year() as usize,
+            complete: false
+        }
+    }
+
+    /// Borrow the year the season took place
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::LeagueSeason;
+    ///
+    /// let my_league_season = LeagueSeason::new();
+    /// let my_season_year = my_league_season.year();
+    /// ```
+    pub fn year(&self) -> &usize {
+        &self.year
+    }
+
+    /// Mutably borrow the year the season took place
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::LeagueSeason;
+    ///
+    /// let mut my_league_season = LeagueSeason::new();
+    /// let mut my_season_year = my_league_season.year_mut();
+    /// ```
+    pub fn year_mut(&mut self) -> &mut usize {
+        &mut self.year
+    }
+
+    /// Borrow the value indicating whether the season is complete
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::LeagueSeason;
+    ///
+    /// let my_league_season = LeagueSeason::new();
+    /// let complete = my_league_season.complete();
+    /// ```
+    pub fn complete(&self) -> &bool {
+        &self.complete
+    }
+
+    /// Mutably borrow the value indicating whether the season is complete
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::LeagueSeason;
+    ///
+    /// let mut my_league_season = LeagueSeason::new();
+    /// let mut complete = my_league_season.complete_mut();
+    /// ```
+    pub fn complete_mut(&mut self) -> &mut bool {
+        &mut self.complete
+    }
+}
+
 /// # `League` struct
 ///
 /// A `League` represents a football league. It contains a vector of teams in
@@ -91,7 +171,9 @@ impl LeagueTeam {
 /// currently in-progress, and it contains a vector of past seasons
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Default, Serialize, Deserialize)]
 pub struct League {
-    teams: BTreeMap<usize, LeagueTeam>
+    teams: BTreeMap<usize, LeagueTeam>,
+    current_season: Option<LeagueSeason>,
+    seasons: Vec<LeagueSeason>
 }
 
 impl League {
@@ -106,7 +188,9 @@ impl League {
     /// ```
     pub fn new() -> League {
         League{
-            teams: BTreeMap::new()
+            teams: BTreeMap::new(),
+            current_season: None,
+            seasons: Vec::new()
         }
     }
 
@@ -217,5 +301,137 @@ impl League {
     /// ```
     pub fn team_mut(&mut self, id: usize) -> Option<&mut LeagueTeam> {
         self.teams.get_mut(&id)
+    }
+
+    /// Borrows the past seasons from a `League`
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::League;
+    ///
+    /// // Instantiate a new League
+    /// let my_league = League::new();
+    ///
+    /// // Borrow the past seasons from the League
+    /// let past_seasons = my_league.seasons();
+    /// ```
+    pub fn seasons(&self) -> &Vec<LeagueSeason> {
+        &self.seasons
+    }
+
+    /// Mutably borrows the past seasons from a `League`
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::League;
+    ///
+    /// // Instantiate a new League
+    /// let mut my_league = League::new();
+    ///
+    /// // Borrow the past seasons from the League
+    /// let mut past_seasons = my_league.seasons_mut();
+    /// ```
+    pub fn seasons_mut(&mut self) -> &mut Vec<LeagueSeason> {
+        &mut self.seasons
+    }
+
+    /// Borrows the current season from a `League`
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::{League, LeagueSeason};
+    ///
+    /// // Instantiate a new League
+    /// let my_league = League::new();
+    ///
+    /// // Borrow the current season from the League
+    /// let my_season: &Option<LeagueSeason> = my_league.current_season();
+    /// ```
+    pub fn current_season(&self) -> &Option<LeagueSeason> {
+        &self.current_season
+    }
+
+    /// Mutably borrows the current season from a `League`
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::{League, LeagueSeason};
+    ///
+    /// // Instantiate a new League
+    /// let mut my_league = League::new();
+    ///
+    /// // Borrow the current season from the `League`
+    /// let mut my_season: &mut Option<LeagueSeason> = my_league.current_season_mut();
+    /// ```
+    pub fn current_season_mut(&mut self) -> &mut Option<LeagueSeason> {
+        &mut self.current_season
+    }
+
+    /// Gets the most recent year among the completed seasons
+    fn most_recent_year(&self) -> usize {
+        let mut most_recent_year = 0_usize;
+        let seasons = self.seasons();
+        for season in seasons.iter() {
+            let season_year = season.year();
+            if *season_year > most_recent_year {
+                most_recent_year = season_year.clone();
+            }
+        }
+        most_recent_year
+    }
+
+    /// Creates a new `LeagueSeason` and archives the current `LeagueSeason`
+    /// if the current `LeagueSeason` is complete
+    ///
+    /// ### Example
+    /// ```
+    /// use fbsim_core::league::League;
+    ///
+    /// // Instantiate a new League
+    /// let mut my_league = League::new();
+    ///
+    /// // Create a new season for the new League
+    /// let res = my_league.create_season();
+    /// ```
+    pub fn create_season(&mut self) -> Result<(), String> {
+        // Check if the current season exists
+        if let Some(season) = self.current_season_mut() {
+            // If so, then check if the season is complete
+            if *season.complete() {
+                // If the season is complete, archive and create new season
+                let most_recent_year = season.year();
+                let mut new_season = LeagueSeason::new();
+                let new_year = new_season.year_mut();
+                *new_year = most_recent_year + 1;
+                let old_season = season.clone();
+                *season = new_season;
+                self.seasons.push(old_season);
+                return Ok(());
+            }
+
+            // If the season is not complete, then error
+            return Err(
+                format!(
+                    "Cannot create new season: {}",
+                    "Current season still in progress"
+                )
+            );
+        }
+
+        // Create a new league season
+        let mut new_season = LeagueSeason::new();
+
+        // If the past seasons list is empty then stick with the default year
+        if self.seasons.len() == 0 {
+            self.current_season = Some(new_season);
+            return Ok(());
+        }
+
+        // If the past seasons list is nonempty then update the year
+        let most_recent_year = self.most_recent_year();
+        let new_year = new_season.year_mut();
+        *new_year = most_recent_year + 1;
+        self.current_season = Some(new_season);
+        return Ok(());
     }
 }
