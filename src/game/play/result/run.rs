b7e97c205b8f0f3d788bde6f67af890b80b207cc
whatsacomputertho <ebalcik71@gmail.com>
2025-12-18 15:41:11 -0500

    fix(rushing): fine-tune rushing weights

--- src/game/play/result/run.rs
+++ src/game/play/result/run.rs
@@ -11,15 +11,15 @@ use crate::game::play::PlaySimulatable;
 use crate::game::play::result::{PlayResult, PlayTypeResult, PlayResultSimulator, ScoreResult};
 
 // Mean & std regression for standard rushing play
-const MEAN_YARDS_INTR: f64 = 3.0503791522871384_f64;
-const MEAN_YARDS_COEF: f64 = 0.32550597_f64;
+const MEAN_YARDS_INTR: f64 = 2.5503791522871384_f64; // adjusted -0.5
+const MEAN_YARDS_COEF: f64 = 0.82550597_f64; // adjusted + 0.5
 const STD_YARDS_INTR: f64 = 4.053915588534795_f64;
 const STD_YARDS_COEF_1: f64 = 0.2487578_f64;
 const STD_YARDS_COEF_2: f64 = 0.0593874_f64;
 
 // Mean & std regression for big, non-TD rushing play
-const MEAN_BP_YARDS_INTR: f64 = 15.781025340879893_f64;
-const MEAN_BP_YARDS_COEF: f64 = 6.32805521_f64;
+const MEAN_BP_YARDS_INTR: f64 = 12.781025340879893_f64; // adjusted -3
+const MEAN_BP_YARDS_COEF: f64 = 9.32805521_f64; // adjusted +3
 const STD_BP_YARDS_INTR: f64 = 10.014877063200005_f64;
 const STD_BP_YARDS_COEF_1: f64 = -3.82403981_f64;
 const STD_BP_YARDS_COEF_2: f64 = 7.60215528_f64;
