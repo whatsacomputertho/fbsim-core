9742924611f7b18e34c6ba0f1b6bd19a8c6091be
whatsacomputertho <ebalcik71@gmail.com>
2025-12-18 16:35:34 -0500

    fix(pass): fine-tune model weights

--- src/game/play/result/pass.rs
+++ src/game/play/result/pass.rs
@@ -23,7 +23,7 @@ const P_SCRAMBLE_INTR: f64 = 0.004914770911025865_f64;
 const P_SCRAMBLE_COEF: f64 = 0.13433329_f64;
 
 // Short pass probability regression
-const P_SHORT_PASS_INTR: f64 = 0.8910555875020549_f64; // Adjusted + 0.05
+const P_SHORT_PASS_INTR: f64 = 0.9010555875020549_f64; // Adjusted + 0.06
 const P_SHORT_PASS_COEF_1: f64 = -0.0054862949_f64;
 const P_SHORT_PASS_COEF_2: f64 = 0.000050472999_f64;
 
@@ -75,7 +75,7 @@ const SKEW_INT_RETURN_YARDS_COEF_3: f64 = 0.00000700818986_f64;
 
 // Completed pass probability regression
 const P_COMPLETE_INTR: f64 = 0.3039580511583472_f64;
-const P_COMPLETE_COEF: f64 = 0.60872763_f64; // Adjusted + 0.2
+const P_COMPLETE_COEF: f64 = 0.55872763_f64; // Adjusted + 0.15
 
 // Zero yards after catch probability regression
 const P_ZERO_YAC_INTR: f64 = 0.46761265601223527_f64; // Adjusted + 0.3
